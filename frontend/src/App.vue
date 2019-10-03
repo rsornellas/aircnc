24ae3bb59b2653cb25732939cbae1b3c9e31a331
Daniel Schmitz <danieljfa@gmail.com>
2019-10-03 07:57:55 -0300

    Assets e Estilo Css Global

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,3 +3,28 @@
     <router-view/>
   </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+* {
+  margin: 0px;
+  padding: 0px;
+  outline: 0px;
+  box-sizing: border-box;
+}
+html, body, #app {
+  min-height: 100px;
+}
+
+body {
+  background: #000 url('./assets/background.jpg') no-repeat;
+  background-size: cover;
+  -webkit-font-smoothing: antialiased !important;
+}
+
+body, input, button {
+  font-family: 'Roboto', Arial, Helvetica,  sans-serif;
+  font-size: 14px;
+}
+
+</style>
